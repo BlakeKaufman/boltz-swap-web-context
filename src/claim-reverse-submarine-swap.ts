bcbe4810b6bd897365fc1310796f1a780704f41e
Blake Kaufman <68204898+BlakeKaufman@users.noreply.github.com>
2024-05-27 11:28:12 -0400

    Update claim-reverse-submarine-swap.ts

--- src/claim-reverse-submarine-swap.ts
+++ src/claim-reverse-submarine-swap.ts
@@ -47,8 +47,11 @@ export const claimReverseSubmarineSwap = async ({
   })
   const boltzPublicKey = Buffer.from(refundPublicKey, 'hex')
 
+  const zkp = await zkpInit()
+  init(zkp)
+
   // Create a musig signing session and tweak it with the Taptree of the swap scripts
-  const musig = new Musig(await zkpInit(), keyPair, randomBytes(SESSION_ID_BYTES), [boltzPublicKey, keyPair.publicKey])
+  const musig = new Musig(zkp, keyPair, randomBytes(SESSION_ID_BYTES), [boltzPublicKey, keyPair.publicKey])
   const tweakedKey = LiquidTaprootUtils.tweakMusig(musig, SwapTreeSerializer.deserializeSwapTree(swapTree).tree)
 
   // Parse the lockup transaction and find the output relevant for the swap
